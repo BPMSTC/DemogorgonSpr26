d9dc0c3cf8a534a7a7d9be64a9d30a923d4c0e41
MavScript.blu <darienmaverick@outlook.com>
2026-03-06 15:49:45 -0600

    initial commit: project switchover, creation of angular music festival planner boilerplate and completeion of Dem-46: configure core routes, and add a simple layout so everyone can work with the same codebase

---
+++ music-festival-planner/src/app/components/festivals/festivals.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-festivals',
+  standalone: false,
+  templateUrl: './festivals.html',
+  styleUrl: './festivals.css',
+})
+export class Festivals {}
---
+++ music-festival-planner/src/app/components/my-schedule/my-schedule.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-schedule',
+  standalone: false,
+  templateUrl: './my-schedule.html',
+  styleUrl: './my-schedule.css',
+})
+export class MySchedule {}
